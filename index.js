73abd5a7a73dda431b08484f68e5f9c47a5ee68d
Yoma Okeremeta <square.digitals01@gmail.com>
2025-03-20 12:55:02 +0100

    learnt abit about mapping and handling data

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-const hourEl = document.getElementById('hour')
-const minuteEl = document.getElementById('minutes')
-const secondEl = document.getElementById('seconds')
-const ampmEl = document.getElementById('ampm')
-
-function updateClock() {
-  let h = new Date().getHours()
-  let m = new Date().getMinutes()
-  let s = new Date().getSeconds()
-  let ampm = 'AM'
-
-  if (h > 12) {
-    h = h - 12
-    ampm = 'PM'
+const fetchUser = async () => {
+  try {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let post = await response.json()
+    console.log(post)
+  } catch (error) {
+    console.error('Error fetching user', error)
   }
-
-  h = h < 10 ? '0' + h : h
-  m = m < 10 ? '0' + m : m
-  s = s < 10 ? '0' + s : s
-
-  hourEl.innerText = h
-  minuteEl.innerText = m
-  secondEl.innerText = s
-  ampmEl.innerText = ampm
-  setTimeout(() => {
-    updateClock()
-  }, 1000)
 }
 
-updateClock()
+fetchUser()
