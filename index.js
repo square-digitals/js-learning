68cedd79827b31ff5841e6a6e4a1bdead055bb22
Yoma Okeremeta <square.digitals01@gmail.com>
2025-03-20 13:39:34 +0100

    Completed

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-const hourEl = document.getElementById('hour')
-const minuteEl = document.getElementById('minutes')
-const secondEl = document.getElementById('seconds')
-const ampmEl = document.getElementById('ampm')
+const btnEl = document.querySelector('.btn')
 
-function updateClock() {
-  let h = new Date().getHours()
-  let m = new Date().getMinutes()
-  let s = new Date().getSeconds()
-  let ampm = 'AM'
+btnEl.addEventListener('mouseover', (event) => {
+  const x = event.pageX - btnEl.offsetLeft
+  const y = event.pageY - btnEl.offsetTop
 
-  if (h > 12) {
-    h = h - 12
-    ampm = 'PM'
-  }
-
-  h = h < 10 ? '0' + h : h
-  m = m < 10 ? '0' + m : m
-  s = s < 10 ? '0' + s : s
-
-  hourEl.innerText = h
-  minuteEl.innerText = m
-  secondEl.innerText = s
-  ampmEl.innerText = ampm
-  setTimeout(() => {
-    updateClock()
-  }, 1000)
-}
-
-updateClock()
+  btnEl.style.setProperty('--xPos', x + 'px')
+  btnEl.style.setProperty('--yPos', y + 'px')
+})
